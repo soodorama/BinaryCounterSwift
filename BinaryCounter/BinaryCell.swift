4ce48fc8797cb3025edb854b8af186205a0ecaae
Neil Sood <neilsood@Neils-MacBook-Pro.local>
2018-09-11 21:51:33 -0700

    basic storyboard for first prototype cell

---
+++ BinaryCounter/BinaryCell.swift
@@ -0,0 +1,21 @@
+//
+//  BinaryCell.swift
+//  BinaryCounter
+//
+//  Created by Neil Sood on 9/11/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class BinaryCell: UITableViewCell {
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    
+    @IBOutlet weak var binaryLabel: UILabel!
+    
+    
+    
+    
+}
